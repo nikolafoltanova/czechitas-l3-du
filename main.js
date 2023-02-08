b1db9e7328e956e3320fcfd9a9a905fa032292a5
Nikola Foltánová <foltanova@ders.cz>
2023-02-08 21:05:52 +0100

    ukol3_nikolafoltanova

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 // tady je místo pro náš program
+// prvni bod
+let vystupProgramu = document.querySelector('.vystupProgramu')
+let jmeno = prompt('Jaké je tvé křestní jméno?')
+let prijmeni = prompt('Jaké je tvé příjmení?')
+let rok = Number(prompt('Jaký je tvůj rok narození?'))
+let vek = 2023 - rok
+let vstup = jmeno + ' ' + prijmeni + ' ' + vek
+vystupProgramu.innerHTML = vstup
+vystupProgramu.style.fontSize = '30px'
+vystupProgramu.style.fontWeight = 'bold'
 
+// druhy bod
+let barva = prompt('Jaká je tvá oblíbená barva?')
+console.log(barva)
+vystupProgramu.style.color = barva
 
-
-
+let zmena = document.querySelector('.tlacitko')
+function zmenBarvu() {
+    let barva = prompt('Zadej barvu:')
+    vystupProgramu.style.color = barva
+}
